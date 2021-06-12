01bf4f211d6c6a52779203fa9d962938480fcb13
tjohnson1988 <85202888+tjohnson1988@users.noreply.github.com>
2021-06-12 15:59:43 -0400

    completed

--- top_movies.py
+++ top_movies.py
@@ -1,6 +1,16 @@
 movie_list = ['Die Hard', 'Forest Gump', 'Crash', 'Requiem for a Dream', 'Shrek']
 count = 1
 
+print("Here are my favorite movies:")
 for movie in movie_list:
     print(str(count) + ".", movie)
     count += 1
+
+movie_list.append(input("Please add one more movie: "))
+print(movie_list)
+
+count = 2
+print("Here are the even movies: ")
+for movie in [1, 3, 5]:
+    print(str(count) + ".", movie_list[movie])
+    count += 2

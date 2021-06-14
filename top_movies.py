e7a4285f52e663f423235325e8f2d2b02c74bb9a
tjohnson1988 <85202888+tjohnson1988@users.noreply.github.com>
2021-06-14 09:49:35 -0400

    Updated use of range - reduced code by ~8 lines

--- top_movies.py
+++ top_movies.py
@@ -4,35 +4,25 @@ print("   ")
 movie_list = ['Die Hard', 'Forest Gump', 'Crash', 'Requiem for a Dream', 'Shrek']
 count = 1
 
-print("Here are my favorite movies:")
+print("Here are my favorite movies:") 
 for movie in movie_list:
     print(str(count) + ".", movie)
     count += 1
 print("   ")
 print("Part 2. Add a movie to the list based on user input.")
 print("   ")
-movie_list = ['Die Hard', 'Forest Gump', 'Crash', 'Requiem for a Dream', 'Shrek']
-user_movie = [input("Please add one more movie: ")]
-movie_list = movie_list + user_movie
+#movie_list = ['Die Hard', 'Forest Gump', 'Crash', 'Requiem for a Dream', 'Shrek']
+user_movie = input("Please add one more movie: ")
+#avoid list concat
+movie_list.append(user_movie)
 
 print(movie_list)
 
 print("   ")
 print("Part 3. Print ONLY the even INDEXED movies.")
+#Use the range() to iterate through even indexed movies instead of creating new lists
 print("   ")
 
-even_movies = []
-odd_movies = []
-count = 2
-
-
 print("Here are the even movies: ")
-for movie in range(0, len(movie_list)):
-    if movie % 2:
-        even_movies.append(movie_list[movie])
-    else:
-        odd_movies.append(movie_list[movie])
-
-for movie in even_movies:
-    print(str(count) + ".", movie)
-    count +=2
+for movie in range(0, len(movie_list), 2):
+    print((str(movie)) + ".", movie_list[movie])
